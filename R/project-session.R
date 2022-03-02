ae9910119919e5fd13b40bc9263827ad5a454326
cyan20200410 <cyanfong1610@gmail.com>
2022-03-02 09:04:55 +0100

    Added scripts to git

---
+++ R/project-session.R
@@ -0,0 +1,21 @@
+
+# R project management ----------------------------------------------------
+source(here::here("R/package-loading.R"))
+
+
+weight_killos <-100
+
+# Object names
+Day_One
+T2022 <- FALSE
+c2022 <- 9
+
+# Spacing
+x[, 1]
+mean(x, na.rm = TRUE)
+
+height <- feet * 12 + inches
+df$z
+x <- 1:10
+
+library(tidyverse)
